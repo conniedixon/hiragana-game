50cd7ff56fea5d578b464b3259d3485ec09f53f4
connie <conniedixon106@gmail.com>
2020-05-10 15:48:20 +0100

    added handlechange and checkasnwer functions

--- src/components/Hiragana.jsx
+++ src/components/Hiragana.jsx
@@ -12,17 +12,42 @@ class Hiragana extends Component {
       { hiragana: "た", answer: ["ta"] },
       { hiragana: "な", answer: ["na", "nah"] },
     ],
+    currentCard: 1,
+    guess: "",
   };
+
+  handleChange = ({ target: { value } }) => {
+    this.setState(() => {
+      return {
+        guess: value,
+      };
+    });
+  };
+
+  checkAnswer = (event) => {
+    event.preventDefault();
+    let answers = this.state.cards[this.state.currentCard]["answer"];
+    if (answers.includes(this.state.guess)) console.log("correct");
+    // this.state.cards.currentCard.answer.map((answer) => {
+    //   if (answer === this.state.guess) {
+    //     console.log("correct");
+    //   }
+    // });
+  };
+
   render() {
     return (
       <div>
         <h1>Hiragana Test</h1>
         <h2>Type the correct phonetic sound and press enter.</h2>
-        <form>
-          <input type='text'></input>
-          <button type='submit' onClick='checkAnswer'>
-            Submit!
-          </button>
+        <form onSubmit={this.checkAnswer}>
+          <input
+            type='text'
+            minLength='1'
+            onChange={this.handleChange}
+            value={this.state.comment}
+            placeholder='Type your answer here...'></input>
+          <button>Submit!</button>
           <p>Score: {this.state.points}</p>
         </form>
       </div>
