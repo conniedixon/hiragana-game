2f01f350b4f60f5888a5f1553842a3518aba3388
connie <conniedixon106@gmail.com>
2020-05-12 20:03:45 +0100

    Fixed link in Welcome menu

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,23 @@
+/** @format */
+
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+
+class Welcome extends Component {
+  render() {
+    return (
+      <>
+        <h1>Welcome to Hiragana-Test</h1>
+        <h3>Select an option to begin playing!</h3>
+        <Link to='/infinite'>
+          <p>Infinite Mode</p>
+        </Link>
+        <p>
+          for practicing as many hirigana as you want want with no time limits.
+        </p>
+      </>
+    );
+  }
+}
+
+export default Welcome;
